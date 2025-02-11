cc77d68ef0b96deafd3c5fb611704b11b83a80a5
Abdussattar Kassymbekov <abdusattar.kasymbekov@gmail.com>
2025-02-12 05:41:25 +0600

    updated android

--- android/src/main/java/com/example/flutter_rtmp_publisher/FlutterRtmpPublisherPlugin.java
+++ android/src/main/java/com/example/flutter_rtmp_publisher/FlutterRtmpPublisherPlugin.java
@@ -1,49 +1,50 @@
 package com.example.flutter_rtmp_publisher;
 
+import android.util.Log;
 import java.util.List;
 import java.util.ArrayList;
-
-import android.util.Log;
-
+import io.flutter.embedding.engine.plugins.FlutterPlugin;
+import io.flutter.embedding.engine.plugins.FlutterPlugin.FlutterPluginBinding;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
-import io.flutter.plugin.common.PluginRegistry.Registrar;
 
-/** FlutterRtmpPublisherPlugin */
-public class FlutterRtmpPublisherPlugin implements MethodCallHandler {
-    static List<RTMPCamera> cameraList = new ArrayList<RTMPCamera>();
+public class FlutterRtmpPublisherPlugin implements FlutterPlugin, MethodCallHandler {
+    static List<RTMPCamera> cameraList = new ArrayList<>();
+    private MethodChannel channel;
 
-    /** Plugin registration. */
-    public static void registerWith(Registrar registrar) {
-        RTMPCamera.flutterRegistrar = registrar;
-
-        registrar.platformViewRegistry().registerViewFactory("flutter_rtmp_publisher/RTMPCameraPreview",
-                new RTMPCameraPreviewFactory(registrar.messenger()));
-
-        final MethodChannel channel = new MethodChannel(registrar.messenger(), "flutter_rtmp_publisher/method");
-        channel.setMethodCallHandler(new FlutterRtmpPublisherPlugin());
+    @Override
+    public void onAttachedToEngine(FlutterPluginBinding binding) {
+        RTMPCamera.setBinaryMessenger(binding.getBinaryMessenger());
+        binding.getPlatformViewRegistry().registerViewFactory("flutter_rtmp_publisher/RTMPCameraPreview", new RTMPCameraPreviewFactory(binding.getBinaryMessenger()));
+        channel = new MethodChannel(binding.getBinaryMessenger(), "flutter_rtmp_publisher/method");
+        channel.setMethodCallHandler(this);
     }
 
     @Override
     public void onMethodCall(MethodCall call, Result result) {
         switch (call.method) {
-        case "NewRTMPCamera": {
-            Integer id = cameraList.size();
-            cameraList.add(new RTMPCamera(id));
-            result.success(id);
-            break;
+            case "NewRTMPCamera": {
+                Integer id = cameraList.size();
+                cameraList.add(new RTMPCamera(id));
+                result.success(id);
+                break;
+            }
+            case "DisposeRTMPCamera": {
+                result.success("Android " + android.os.Build.VERSION.RELEASE);
+                break;
+            }
+            default: {
+                Log.i("RTMP Publisher Plugin", "Method call error: " + call.method);
+                result.notImplemented();
+                break;
+            }
         }
-        case "DisposeRTMPCamera": {
-            result.success("Android " + android.os.Build.VERSION.RELEASE);
-            break;
-        }
-        default: {
-            Log.i("RTMP Publisher Plugin", "Method call error");
-            result.notImplemented();
-            break;
-        }
-        }
+    }
+
+    @Override
+    public void onDetachedFromEngine(FlutterPluginBinding binding) {
+        channel.setMethodCallHandler(null);
     }
 }
